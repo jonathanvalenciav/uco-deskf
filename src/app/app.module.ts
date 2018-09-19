8845b398be545e35eac6b08c07388e21047447b8
Jonathan Alexis Valencia <alexisvalenciav@gmail.com>
2018-09-18 21:46:40 -0500

    Adding new and basic components to start collaboration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule
