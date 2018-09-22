d6336d2883b425d13fb0c819432c726054883b5d
Jonathan Alexis Valencia <alexisvalenciav@gmail.com>
2018-09-22 18:28:02 -0500

    Editing the forms body to make invocation.

--- src/app/form-new-project/form-new-project.component.ts
+++ src/app/form-new-project/form-new-project.component.ts
@@ -5,8 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './form-new-project.component.html',
   styleUrls: ['./form-new-project.component.css']
 })
-export class FormNewProjectComponent implements OnInit {
 
+export class FormNewProjectComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
